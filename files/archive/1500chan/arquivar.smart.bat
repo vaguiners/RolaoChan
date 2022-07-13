07d558e10a2781e482c0b620d750b985f22a06e5
vaguiners <vaguiners@live.com>
2022-07-13 05:25:54 -0300

    a

--- files/archive/1500chan/arquivar.smart.bat
+++ files/archive/1500chan/arquivar.smart.bat
@@ -4,9 +4,9 @@ chcp 65001
 :baixar
 cls
 echo Baixando tópicos...
-for /F %%A in ('curl -s https://1500chan.org/b/threads.json ^| jq .[].threads[].no') do (
+for /F %%A in ('curl -b cookies.txt -s https://1500chan.org/b/threads.json ^| jq .[].threads[].no') do (
 	::curl -s https://1500chan.org/b/res/%%A.json | jq [.posts[].com_nomarkup] | gzip -f -9>threads\%%A.json.gz
-	curl -s https://1500chan.org/b/res/%%A.json | jq [.posts[].com_nomarkup] >%temp%\%%A.json 2> nul
+	curl -b cookies.txt -s https://1500chan.org/b/res/%%A.json | jq [.posts[].com_nomarkup] >%temp%\%%A.json 2> nul
 	echo %%A: !errorlevel!:%errorlevel%
 	if !errorlevel!==0 move /y %temp%\%%A.json threads 1> nul 2> nul
 	del /q %temp%\%%A.json 1> nul 2> nul
